825eef992d876a9344357b0468e4796925b584cd
MD Rakib Hasan <dev.rakibhasan1@gmail.com>
2025-11-05 12:43:57 +0600

    complete practise module 3

---
+++ M03V07.js
@@ -0,0 +1,147 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+
+        //? If the linked list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            //? If the linked list is not empty
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+
+        //? If the linked list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            //? If the linked list is not empty
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        return this;
+    }
+
+    insert(index, value) {
+        if (index < 0 || index > this.length) {
+            console.error("Index out of bound");
+            return undefined;
+        }
+
+        // If he insert is in the start of the linked list
+        if (index === 0) {
+            return this.prepend(value);
+        }
+
+        // If the insert is in the end of the list
+        if (index === this.length) {
+            return this.append(value);
+        }
+
+        // If the inser is in the middle
+
+        // find the leading node
+
+        const leadingNode = this._traverseToIndex(index - 1);
+        const holdingNode = leadingNode.next;
+
+        const newNode = new Node(value);
+
+        leadingNode.next = newNode;
+        newNode.next = holdingNode;
+
+        this.length++;
+
+
+    }
+
+    remove(index) {
+        if (index === 0) {
+            const removedItem = this.head.value;
+            this.head = this.head.next;
+
+            if (this.length === 1) {
+                this.tail = null;
+            }
+            this.length--;
+            return removedItem;
+        }
+
+        const leadingNode = this._traverseToIndex(index - 1);
+        const nodeToRemove = leadingNode.next;
+
+        leadingNode.next = nodeToRemove.next;
+
+        if (leadingNode.next === null) {
+            this.tail = leadingNode;
+        }
+        return nodeToRemove.value;
+    }
+
+
+    // ? Private Helper method
+    _traverseToIndex(index) {
+        let count = 0;
+        let currentNode = this.head;
+
+        while (count !== index) {
+            currentNode = currentNode.next;
+            count++;
+        }
+        return currentNode;
+    }
+
+    print() {
+
+        const arr = [];
+        let currentNode = this.head;
+
+        while (currentNode !== null) {
+            arr.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(arr.join(" -> "), "-> null");
+    }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.append("A");
+linkedList.append("B");
+linkedList.append("c");
+linkedList.append("d");
+
+
+
+linkedList.print();
+
+linkedList.remove(0);
+linkedList.remove(1);
+
+
+linkedList.print();
